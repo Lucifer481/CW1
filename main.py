539380e72c041e3ff19e8424b73d82a8cc81992c
Lucifer481 <novemberking08@gmail.com>
2024-02-05 09:35:00 +0545

    Importing all modules

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import hashlib
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from cryptography.fernet import InvalidToken
+from cryptography.fernet import Fernet
+from Cryptodome.Cipher import AES
+import tkinter.simpledialog
+import unittest
