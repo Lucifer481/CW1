0d943050136504bc113d5b30f3fa214bf4fe7df8
Lucifer481 <novemberking08@gmail.com>
2024-02-05 10:09:39 +0545

    encrypted

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ from cryptography.fernet import Fernet
 from Cryptodome.Cipher import AES
 import tkinter.simpledialog
 import unittest
+from PIL import Image, ImageTk
+
+# Generate a key and encrypt the message
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+cipher_text = cipher_suite.encrypt(b'This is my secret message')
+
+# Save the key to a file
+with open('filekey.key', 'wb') as filekey:
+    filekey.write(key)
+
+# Save the encrypted message to a file
+with open('encrypted_message.txt', 'wb') as file:
+    file.write(cipher_text)
+
+# Version control
+__version__ = "1.1"
+
