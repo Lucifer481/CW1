c719923f66a5e8b23697dd96fa2dafdd2beb1193
Lucifer481 <novemberking08@gmail.com>
2024-02-06 08:16:55 +0545

    unit test

--- main.py
+++ main.py
@@ -27,3 +27,22 @@ with open('encrypted_message.txt', 'wb') as file:
 # Version control
 __version__ = "1.1"
 
+# Encryption 
+
+class EncryptionTool:
+    def __init__(self, user_file, user_key, user_salt):
+        self.user_file = user_file
+        self.input_file_size = os.path.getsize(self.user_file)
+        self.chunk_size =1024
+        self.total_chunks = (self.input_file_size // self.chunk_size) + 1
+        self.user_key = bytes(user_key, "utf-8")
+        self.user_salt = bytes(user_key[::-1], "utf-8")
+        self.file_extension = self.user_file.split(".")[-1]
+        self.hash_type ="SHA256"
+        self.encrypt_output_file = ".".join(self.user_file.split)(".")[:-1] \
+                                   + "." + self.file_extension + ".seyp"
+        self.decrypt_output_file = self.user_file[:-5].split(".")
+        self.decrypt_output_file = ".".join(self.decrypt_output_file[:-1]) \
+                                   + "__deseypted__." + self.decrypt_output_file[-1]
+        self.hashed_key_salt = dict()
+        self.hash_key_salt()
---
+++ test_encryption_tool.py
@@ -0,0 +1,10 @@
+import unittest
+from your_main_script import EncryptionTool
+
+class TestEncryptionTool(unittest.TestCase):
+    def test_encryption_decryption(self):
+        # Example test case
+        # ...
+
+if __name__ == '__main__':
+    unittest.main()
